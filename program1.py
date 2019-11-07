2923649f3a53419127d92c8243dad5812844f7b7
Budi susmiati <57002773+Budisusmiati@users.noreply.github.com>
2019-11-07 23:02:42 +0700

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,10 @@
+a=100000000
+sum=0
+b=0
+laba = [int(0), int(0), int(a) * .1, int (a) * .1, int(a) * .5, int(a) * .5, int(a) * .5, int(a) * .2]
+print("Modal investasi awal seorang pengusaha:", a)
+for i in laba :
+    sum = sum+i
+    b+=1
+    print("Laba bulan ke-",b,"sebesar: ", i)
+print("Total laba Pengusaha adalah:", sum)
